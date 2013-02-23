fc1e3ae616ef994cb2a6e13ac101311c1dfdb29c
Josh DeWitt <jdewitt@unomaha.edu>
2013-02-23 12:42:28 -0600

    Code for character queue in memory.
    Initialize the queue when keyboard is initialized.
    When interrupts are generated from keyboard, character added to queue.
    int 16h will dequeue from this queue.

--- KeyInt.asm
+++ KeyInt.asm
@@ -1,17 +1,65 @@
 ;Keyboard service requests
 
+;inputs:    al - the new character
+;outputs:   queue is updated with new character if possible
+;           modifies ds to be the RAM segment
+mInsertIfNotFull macro
+   push cx
+   
+   xor bx,bx         ;change to RAM segment
+   mov ds,bx
+
+   mov bl,[keyboardPointers]
+   mov bh,bl
+   and bx,0f00fh     ;clear duplicate nibbles
+   mov cl,4
+   shr bh,cl         ;bh - head, bl - tail
+
+   ;head always points to open location
+   push bx
+   mov bl,bh
+   xor bh,bh
+   mov [bx + keyboardQueue],al
+   pop bx
+
+   inc bh
+   cmp bh,0fh        ;see if pointing to end of array
+   jne validNewBh
+   xor bh,bh         ;wrap around to beginning of array
+
+validNewBh:
+   ;if head now equals tail, the queue is full, don't update pointers
+   cmp bh,bl
+   je characterInsertComplete
+
+   shl bh,cl         ;re-construct pointer byte
+   or bl,bh
+   mov [keyboardPointers],bl
+   
+characterInsertComplete:
+   pop cx
+#em
+
 scanAsciiTable db '0123456789ABCDEF'
 ;called by hardware when character is available
 ;inputs:    none
-;outputs:   ah - key scan code
-;           al - ASCII character
+;outputs:   queue is updated with new character
 int09h proc far
-   mov ah,[keyboardData]
-   mov al,ah
-   mov bx,offset scanAsciiTable
-   xlat              ;convert al to ASCII
-   mov B[keyboardCommand],11100000b ;end the interrupt request
+   push bx,ds
+   mov bx,keyboardSegment
+   mov ds,bx
 
+   mov B[keyboardCommand],01000000b ;setup to read 8279
+
+   xor bx,bx
+   mov bl,[keyboardData]
+   mov al,cs:[bx + offset scanAsciiTable]
+
+   mov B[keyboardCommand],11100000b ;end the 8279 interrupt request
+
+   mInsertIfNotFull
+
+   pop ds,bx
    iret
 int09h endp
 
@@ -52,13 +100,17 @@ pReadCharacter endp
 ;inputs:    none
 ;outputs:   none, 8279 initialized
 pInitializeKeyboard proc near
-   push ds
+   push ax,ds
    mov ds,keyboardSegment
 
    mov B[keyboardCommand],00000001b ;set to decoded scan keyboard
    mov B[keyboardCommand],00111001b ;set to 25 prescaler to get 100kHz clock
    mov B[keyboardCommand],01000000b ;setup to read character queue
+  
+   xor ax,ax         ;set up head and tail pointer for queue
+   mov ds,ax
+   mov B[keyboardQueue],0Eh
 
-   pop ds
+   pop ds,ax
    ret
 pInitializeKeyboard endp
