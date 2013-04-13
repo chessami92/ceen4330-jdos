8e05f1627c3c70e512d3e6bc2ca02d890143d392
Josh DeWitt <jdewitt@unomaha.edu>
2013-04-13 13:02:06 -0600

    Added in necessary comments to input procedures.

--- Input.asm
+++ Input.asm
@@ -7,6 +7,7 @@
 ;           ah - msb is set if user hit space
 ;              second msb is set if user hit enter
 ;              third msb is set if user hit backspace
+;calls:     int 21h - input without echo, int 10h - print character function
 pInputOneHex proc
    push cx,dx
 
@@ -17,38 +18,38 @@ attemptInputCharacter:
 checkPressedSpace:
    cmp al,' '
    jne checkPressedEnter
-   mov ah,80h
+   mov ah,80h        ;set proper return code for pressing space
    jmp doneInputOneHex
 checkPressedEnter:
    cmp al,0ah
    jne checkPressedBackspace
    push dx
    mov cl,2
-   shl dh,cl
+   shl dh,cl         ;see if pressing return is allowed
    pop dx
    jnc attemptInputCharacter
-   mov ah,40h
+   mov ah,40h        ;set proper return code for pressing enter
    jmp doneInputOneHex
 checkPressedBackspace:
    cmp al,08h
    jne normalInputHex
    push dx
    mov cl,3
-   shl dh,cl
+   shl dh,cl         ;see if pressing backspace is allowed
    pop dx
    jnc attemptInputCharacter
    mOutputCharacter al
-   mov ah,20h
+   mov ah,20h        ;set proper return code for pressing backspace
    jmp doneInputOneHex
    
 normalInputHex:
    push dx
    mov cl,4
-   shl dh,cl
+   shl dh,cl         ;check if normal characters (0-f) are allowed
    pop dx
    jc attemptInputCharacter
    
-   call pUpperCaseAl
+   call pUpperCaseAl ;convert to uper case if necessary
    cmp al,'0'        ;check for 0-9
    jb attemptInputCharacter
    cmp al,'9'
@@ -65,10 +66,10 @@ checkLetters:
 
 validInput:
    and al,0fh        ;clear upper nibble
-   cmp al,dl
+   cmp al,dl         ;make sure that inputted character is not greater than max allowed character
    ja attemptInputCharacter
    
-   shl dh,1
+   shl dh,1          ;see if character should be echoed
    jnc doneInputOneHex
    mov dl,al
    mHexToAscii dl
@@ -84,30 +85,31 @@ pInputOneHex endp
 ;outputs:   bx - the inputted characters in hex format
 ;              characters echoed to screen
 ;           cf - set if user hit space to exit
+;calls:     pInputOneHex
 pInputManyHex proc near
    push ax,cx,dx
    
    xor bx,bx
-   mov dl,ch
+   mov dl,ch         ;for pInputOneHex, dl is max allowed hex value
    mov ch,cl         ;to know if at beginning of input
    
 inputNextCharacter:
-   mov dh,0a0h
+   mov dh,0a0h       ;normal function code if not at end or beginning of input
    cmp cl,ch
    jne notAtBeginningOfInput
-   and dh,0dfh        ;disable backspace
+   and dh,0dfh       ;disable backspace
 notAtBeginningOfInput:
    cmp cl,0
    jne notAtEndOfInput
-   and dh,7fh       ;disable echo
+   and dh,7fh        ;disable echo
    or dh,50h         ;allow for enter, disable normal characters
 notAtEndOfInput:
-   call pInputOneHex
-   shl ah,1
-   jc doneInputManyHex
-   shl ah,1
+   call pInputOneHex ;get next character
+   shl ah,1          ;see if user hit space
+   jc doneInputManyHex;return with carry flag set
+   shl ah,1          ;see if user hit enter
    jc doneInputManyHexClearCarry
-   shl ah,1
+   shl ah,1          ;see if user hit backspace
    jnc backspaceNotPressed
    inc cl
    shr bx,1          ;clear out deleted character
